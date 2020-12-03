eea83b0055c236fcce49c1a13018ff6adba6748c
Matt Leeke <matthewleeke@hastingsdirect.com>
2020-12-03 00:00:26 +0000

    Initial commit

---
+++ List-CAPolicies.ps1
@@ -0,0 +1,40 @@
+﻿# Variables
+
+$TenantID = "<TENANT-ID>"
+$ClientID = "<CLIENT-ID"
+$ClientSecret = "<CLIENT-SECRET>"
+
+# Create a hashtable for the body, the data needed for the token request
+# The variables used are explained above
+
+$Body = @{
+    'tenant' = $TenantId
+    'client_id' = $ClientId
+    'scope' = 'https://graph.microsoft.com/.default'
+    'client_secret' = $ClientSecret
+    'grant_type' = 'client_credentials'
+}
+
+# Assemble a hashtable for splatting parameters, for readability
+# The tenant id is used in the uri of the request as well as the body
+
+$Params = @{
+    'Uri' = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
+    'Method' = 'Post'
+    'Body' = $Body
+    'ContentType' = 'application/x-www-form-urlencoded'
+}
+
+$AuthResponse = Invoke-RestMethod @Params
+
+# Invoke request
+
+$Headers = @{
+    'Authorization' = "Bearer $($AuthResponse.access_token)"
+}
+
+$Result = Invoke-RestMethod -Uri 'https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies' `
+    -Headers $Headers `
+    -Method GET
+
+$Result.value.id[0]
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # conditional-access
 Powershell retrieval and creation of Conditional Access policies
+
+App registration required the following permissions in Microsoft Graph:
+Application.Read.All
+Directory.Read.All
+Policy.Read.All
+Policy.ReadWrite.ConditionalAccess
